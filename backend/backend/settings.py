80cbcfa23147d0955f33ae6d24cc21f1d66474c7
tds455 <92926829+tds455@users.noreply.github.com>
2024-03-01 17:32:06 +0900

    build models in models.py using inspectdb

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'7F44839',
+        'HOST': 'localhost',
+        'PORT': 60000,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
